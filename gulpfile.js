99b3c0c89079af6f8c09abec795de8f406c9d933
dong.keke <dong.keke@tongbanjie.com>
2016-05-28 09:22:48 +0800

    添加gulp配置

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var uglify = require('gulp-uglify');
 var del = require('del');
+var connect = require('gulp-connect');
 
 var paths = {
   build: './build/' + jsonObj.name + '/' + jsonObj.version
@@ -18,6 +19,7 @@ gulp.task('clean',function(cb){
 gulp.task('copy-html', function() {
  	gulp.src('./src/**/*.html')
     .pipe(gulp.dest(paths.build))
+    .pipe(connect.reload())
 });
 
 gulp.task('stylCompile', function() {
@@ -27,9 +29,27 @@ gulp.task('stylCompile', function() {
     }))
     // .pipe(stylus())
     .pipe(gulp.dest(paths.build))
+    .pipe(connect.reload())
 });
 
+gulp.task('reload', function () {
+  gulp.src('./build/**/*.*')
+    .pipe(connect.reload());
+});
 
+gulp.task('watch', function () {
+  gulp.watch(['./src/**/*.html'], ['copy-html']);
+  gulp.watch(['./src/**/bundle.styl'], ['stylCompile']);
+  gulp.watch(['./build/**/*.*'], ['reload']);
+});
 
+gulp.task("connect",function(){
+  connect.server({
+    root : "./build",
+    host: "localhost",
+    port : 8082,
+    livereload: true
+  });
+});
 
-gulp.task('default', ['stylCompile','copy-html']);
+gulp.task('default', ['connect','watch']);
